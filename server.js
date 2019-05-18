c9e33676e516845f30292d566c3e9b1d85d2f463
mathurdevanshi <mathurdevanshi@gmail.com>
2019-05-17 19:54:58 -0700

    got env update gitignore

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 var mysql = require("mysql");
 const path = require('path');
 const router = express.Router();
-
+require('dotenv').config();
 
 app.use(express.json());
 var bodyParser = require('body-parser');
@@ -23,8 +23,8 @@ app.use(express.static('public'));
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "webuser",
-  password: "UCR",
+  user: process.env.NAME_OF_USER,
+  password: process.env.PASSWORD,
   database: "bridge_the_gap_db"
 });
 connection.connect(function (err) {
@@ -39,8 +39,6 @@ connection.connect(function (err) {
 // var fs = require('fs');
 /////////////////////////// API ROUTES AND DIRECTION ////////////////////////
 router.get("/", function (req, res) {
-  console.log("You are on the main page")
-  console.log(__dirname);
   res.sendFile(path.join(__dirname + '/public/html/index.html'));
 
 });
@@ -71,12 +69,10 @@ app.use('/', router);
 // Obtaining form info and displaying results.
 
 app.post("/api/homeless", function (req, res) {
-  console.log("POST", req.body);
   var sql = "INSERT INTO homeless";
   sql += " (fullName, email, phoneNumber, mailingAddress, category, explanation, quantity)";
   sql += " VALUES ";
   sql += " (?,?,?,?,?,?,?);";
-  console.log(sql);
   connection.query(sql, [
     req.body.fullName,
     req.body.email,
@@ -90,10 +86,8 @@ app.post("/api/homeless", function (req, res) {
       console.log("OH DEAR GOD SO MUCH IS GONE BAD!! WHY IS THERE SO MUCH BLOOD??");
       throw err;
     };
-    console.log(sqlResult);
     // fetch matching agencies from database
     var sql = "SELECT * FROM agency_supply WHERE category = '" + req.body.category + "';"
-    console.log(sql);
     // var sql = "SELECT * FROM agency_supply where agency_supply.category = " + req.body.category + ";"
     connection.query(sql, function (err, sqlResult) {
       if (err) {
@@ -106,7 +100,6 @@ app.post("/api/homeless", function (req, res) {
   });
 });
 app.post("/api/agency_supply", function (req, res) {
-  console.log("POST", req.body);
   var sql = "INSERT INTO agency_supply";
   sql += " (fullName, email, phoneNumber, mailingAddress, category, explanation, quantity)";
   sql += " VALUES ";
@@ -124,10 +117,8 @@ app.post("/api/agency_supply", function (req, res) {
       console.log("OH DEAR GOD SO MUCH IS GONE BAD!! WHY IS THERE SO MUCH BLOOD??");
       throw err;
     };
-    console.log(sqlResult);
     // fetch matching agencies from database
     var sql = "SELECT * FROM homeless WHERE category = '" + req.body.category + "';"
-    console.log(sql);
     // var sql = "SELECT * FROM agency_supply where agency_supply.category = " + req.body.category + ";"
     connection.query(sql, function (err, sqlResult) {
       if (err) {
@@ -141,7 +132,6 @@ app.post("/api/agency_supply", function (req, res) {
 });
 
 app.post("/api/agency_need", function (req, res) {
-  console.log("POST", req.body);
   var sql = "INSERT INTO agency_need";
   sql += " (fullName, email, phoneNumber, mailingAddress, category, explanation, quantity)";
   sql += " VALUES ";
@@ -159,10 +149,8 @@ app.post("/api/agency_need", function (req, res) {
       console.log("OH DEAR GOD SO MUCH IS GONE BAD!! WHY IS THERE SO MUCH BLOOD??");
       throw err;
     };
-    console.log(sqlResult);
     // fetch matching volunteer info from database
     var sql = "SELECT * FROM volunteer WHERE category = '" + req.body.category + "';"
-    console.log(sql);
     // var sql = "SELECT * FROM agency_supply where agency_supply.category = " + req.body.category + ";"
     connection.query(sql, function (err, sqlResult) {
       if (err) {
@@ -176,7 +164,6 @@ app.post("/api/agency_need", function (req, res) {
 });
 
 app.post("/api/volunteer", function (req, res) {
-  console.log("POST", req.body);
   var sql = "INSERT INTO volunteer";
   sql += " (fullName, email, phoneNumber, mailingAddress, category, explanation, quantity)";
   sql += " VALUES ";
@@ -194,10 +181,8 @@ app.post("/api/volunteer", function (req, res) {
       console.log("OH DEAR GOD SO MUCH IS GONE BAD!! WHY IS THERE SO MUCH BLOOD??");
       throw err;
     };
-    console.log(sqlResult);
     // fetch matching agencies in need from database
     var sql = "SELECT * FROM agency_need WHERE category = '" + req.body.category + "';"
-    console.log(sql);
     // var sql = "SELECT * FROM agency_supply where agency_supply.category = " + req.body.category + ";"
     connection.query(sql, function (err, sqlResult) {
       if (err) {
